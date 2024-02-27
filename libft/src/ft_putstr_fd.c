ea8b2e0d7b486a8467e76b9ab54a3f563bd5f43c
Ayse <150714002+AyseAtan@users.noreply.github.com>
2024-02-27 19:32:52 +0100

    Update ft_putstr_fd.c

--- libft/src/ft_putstr_fd.c
+++ libft/src/ft_putstr_fd.c
@@ -20,24 +20,3 @@ void	ft_putstr_fd(char *s, int fd)
 		s++;
 	}
 }
-/*
-
-static void test_ft_putstr_fd()
-{
-    // Open a file or use another valid file descriptor
-    int fileDescriptor = 1; // 1 represents standard output (stdout)
-
-    // Characters to write
-    char str[] = "Teststring.";
-
-    // Write each character to the file descriptor using ft_putchar_fd
-    ft_putstr_fd(str, fileDescriptor);
-
-}
-
-int main()
-{
-    test_ft_putstr_fd();
-
-    return 0;
-}*/
