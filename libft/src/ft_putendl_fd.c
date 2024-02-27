7c8f6fa61ea2c5222bf9790e3b08b98ed00cd81d
Ayse <150714002+AyseAtan@users.noreply.github.com>
2024-02-27 19:32:27 +0100

    Update ft_putendl_fd.c

--- libft/src/ft_putendl_fd.c
+++ libft/src/ft_putendl_fd.c
@@ -21,24 +21,3 @@ void	ft_putendl_fd(char *s, int fd)
 	}
 	write (fd, "\n", 1);
 }
-/*
-
-static void test_ft_putendl_fd()
-{
-    // Open a file or use another valid file descriptor
-    int fileDescriptor = 1; // 1 represents standard output (stdout)
-
-    // Characters to write
-    char str[] = "Teststring..";
-
-    // Write each character to the file descriptor using ft_putchar_fd
-    ft_putendl_fd(str, fileDescriptor);
-
-}
-
-int main()
-{
-    test_ft_putendl_fd();
-
-    return 0;
-}*/
