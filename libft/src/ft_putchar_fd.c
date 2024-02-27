87882c7964cd5914ee2e781031b719ce2d1a90a0
Ayse <150714002+AyseAtan@users.noreply.github.com>
2024-02-27 19:32:13 +0100

    Update ft_putchar_fd.c

--- libft/src/ft_putchar_fd.c
+++ libft/src/ft_putchar_fd.c
@@ -16,27 +16,3 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
-/*
-
-static void test_ft_putchar_fd()
-{
-    // Open a file or use another valid file descriptor
-    int fileDescriptor = 1; // 1 represents standard output (stdout)
-
-    // Characters to write
-    char characters[] = {'H', 'e', 'l', 'l', 'o', '\n'};
-    int size = sizeof(characters);
-    // Write each character to the file descriptor using ft_putchar_fd
-    for (int i = 0; i < size; i++)
-    {
-        ft_putchar_fd(characters[i], fileDescriptor);
-    }
-}
-
-int main()
-{
-
-    test_ft_putchar_fd();
-
-    return 0;
-}*/
